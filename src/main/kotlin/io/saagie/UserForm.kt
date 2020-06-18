eccf33e63ca995bb60366789b23ed6cffd6abbfa
Richard Capraro <richard.capraro@saagie.com>
2020-06-18 09:32:04 +0200

    refacto

--- src/main/kotlin/io/saagie/UserForm.kt
+++ src/main/kotlin/io/saagie/UserForm.kt
@@ -4,10 +4,10 @@ import arrow.core.*
 import arrow.core.extensions.nonemptylist.semigroup.semigroup
 import arrow.core.extensions.validated.applicative.applicative
 
-sealed class Error( val cause: String) {
-    object NameTooShort: Error("Name is too short")
-    object PasswordNotStrongEnough: Error("Password is not strong enough")
-    object TooYoung: Error("User should be older than 18")
+sealed class Error(val cause: String) {
+    object NameTooShort : Error("Name is too short")
+    object PasswordNotStrongEnough : Error("Password is not strong enough")
+    object TooYoung : Error("User should be older than 18")
 }
 
 data class UserForm(val userName: String, val password: String, val age: Int) {
@@ -19,23 +19,22 @@ data class UserForm(val userName: String, val password: String, val age: Int) {
             .size >= 2
 
     fun validateUserName(): ValidatedNel<Error, String> =
-        if(userName.length < 2) {
-
+        if (userName.length < 2) {
             Error.NameTooShort.invalidNel()
         } else userName.validNel()
 
     fun validatePassword(): ValidatedNel<Error, String> =
-       if (isComplexPassword()) {
-           password.validNel()
-       } else {
-           Error.PasswordNotStrongEnough.invalidNel()
-       }
+        if (isComplexPassword()) {
+            password.validNel()
+        } else {
+            Error.PasswordNotStrongEnough.invalidNel()
+        }
 
     fun validateAge(): ValidatedNel<Error, Int> =
-       if (age <18) {
+        if (age < 18) {
             Error.TooYoung.invalidNel()
         } else {
-           age.validNel()
+            age.validNel()
         }
 }
 
@@ -46,24 +45,21 @@ fun getValidatedUserFromUserForm(userForm: UserForm): Either<Nel<Error>, User> =
         userForm.validateUserName(),
         userForm.validatePassword(),
         userForm.validateAge()
-    ) {
-        val userName = it.a
-        val password = it.b
-        val age = it.c
-        User(userName, password, age)
-    }.fix().toEither()
+    ) { (userName, password, age) -> User(userName, password, age) }
+        .fix()
+        .toEither()
 
 fun displayUserOrErrors(userForm: UserForm) =
     getValidatedUserFromUserForm(userForm)
         .mapLeft { error -> println("There were errors validating the UserForm:${error.map { it.cause }}") }
-        .map { println("Getting a fully validated User: $it")}
+        .map { println("Getting a fully validated User: $it") }
 
 fun main() {
     val userForm = UserForm("a", "password", 30)
 
     displayUserOrErrors(userForm)
 
-    val userForm2 = UserForm("Richard", "S@@agie!", 45 )
+    val userForm2 = UserForm("Richard", "S@@agie!", 45)
 
     displayUserOrErrors(userForm2)
 }
